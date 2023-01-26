d00aa7364797b8fb73f29274adad2de272b27989
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-01-26 17:54:43 +0200

    Added Header component, removed unnecessary tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src="./logo.svg" className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <Header />
         </div>
     );
 }
