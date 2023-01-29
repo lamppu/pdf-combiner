6bb65dbe031c636fd17629a2393e93082a0f69d2
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-01-29 02:42:07 +0200

    Added state variable mergedPDFUrl, added CombineFilesButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { ActualFileObject, FilePondInitialFile } from 'filepond';
 import React, { useState } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
+import CombineFilesButton from './Components/CombineFilesButton/CombineFilesButton';
 import PDFUploader from './Components/PDFUploader/PDFUploader';
 
 function App() {
-    const [files, setFiles] = useState<(FilePondInitialFile | ActualFileObject | Blob | string)[]>([]);
+    const [files, setFiles] = useState<Blob[]>([]);
+    const [mergedPDFUrl, setMergedPdfUrl] = useState<string>('');
     return (
         <div className="App">
             <Header />
             <PDFUploader files={files} onFilesChange={setFiles} />
+            <CombineFilesButton files={files} onMergedPDFUrlChange={setMergedPdfUrl} />
         </div>
     );
 }
